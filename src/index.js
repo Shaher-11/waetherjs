f8876cdd36376183351cb9d53b38ed5655573db2
Shaher-11 <shahershamroukh@gmail.com>
2021-06-30 14:57:55 +0200

    Add El

--- src/index.js
+++ src/index.js
@@ -16,3 +16,18 @@ function getWeather() {
 
 // get weather on dom load
 document.addEventListener('DOMContentLoaded', getWeather);
+
+// change location event
+
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+  const city = document.getElementById('city').value;
+  const state = document.getElementById('state').value;
+
+  weather.changeLocation('Miami', 'FL');
+
+  // get and display weather
+  getWeather();
+
+  // close modal
+  $('#locModal').modal('hide');
+});
